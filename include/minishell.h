bf9a348da0cba5045c4c8e6995e6613c55f8ea19
Nuno Miguel Martins Machado <numartin@c1r11s11.42porto.com>
2023-05-16 17:33:36 +0100

    starting handling signals

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,14 @@
 /*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:37:18 by numartin          #+#    #+#             */
-/*   Updated: 2023/05/16 14:37:18 by numartin         ###   ########.fr       */
+/*   Updated: 2023/05/16 16:24:58 by numartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
