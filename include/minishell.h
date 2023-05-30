cae6c9251902f2999b82a3f449bb62f0cb268dc4
Nuno Miguel Martins Machado <numartin@c1r11s11.42porto.com>
2023-05-30 18:07:32 +0100

    refactor code. fix norm

--- include/minishell.h
+++ include/minishell.h
@@ -3,29 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jodos-sa <jodos-sa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:37:18 by numartin          #+#    #+#             */
-/*   Updated: 2023/05/17 18:18:26 by jodos-sa         ###   ########.fr       */
+/*   Updated: 2023/05/30 18:06:30 by numartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <signal.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include <fcntl.h>
 # include <errno.h>
 # include <sys/wait.h>
-#include "libft.h"
+# include "libft.h"
 
-
+/* --------------------------------- Signals -------------------------------- */
+void	handle_ctrl_c(int signo);
+int		handle_ctrl_d(char *cmd);
+int		typed_exit(char *cmd);
 
 char	*path(char *cmd, char **envp);
-
 void	free_split(char **args);
-
 void	last_cmd(char *comand, char **envp);
+
+#endif
