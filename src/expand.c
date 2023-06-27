c215e5d9ce64eeb25d4360474f0f55635b2bd6d6
nunom4chado <nunom4chado@gmail.com>
2023-06-27 17:09:19 +0100

    expand inside double quotes

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:34:38 by numartin          #+#    #+#             */
-/*   Updated: 2023/06/27 14:16:30 by numartin         ###   ########.fr       */
+/*   Updated: 2023/06/27 17:08:22 by numartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,14 +55,6 @@ char	*expand(t_state *state)
 	i = 0;
 	while (state->cmd[i])
 	{
-		if (state->cmd[i] == '~')
-		{
-			if (ft_getenv("HOME=", state))
-			{
-				state->cmd = ft_expand(state->cmd, i, ft_getenv("HOME=", state), 0);
-				return(expand(state));
-			}
-		}
 		if (state->cmd[i] == '$')
 		{
 			envari = ft_read_until(state->cmd + i + 1);
@@ -104,54 +96,6 @@ void ft_tilde_expand(t_token *token, t_state *state)
 }
 
 /**
- * Expand variables and $? (previous exit status)
- * 
- * In this implementation, if the next character after '$' is not
- * '_', alpha char or '?', both the first and second chars will be
- * skipped and not saved
-*/
-void ft_variable_expand(t_token *token, t_state *state)
-{
-	char	*ptr;
-	int		singlequote;
-
-	singlequote = 0;
-	ptr = token->word;
-
-	/**
-	 * $a = "Music Pictures"
-	 * $b = " Music Pictures"
-	 * $c = "Music Pictures "
-	 * 
-	 * $a"Videos" = ['Music', 'PicturesVideos']
-	 * $a" Videos" = ['Music', 'Pictures Videos']
-	 * $c"Videos" = ['Music', 'Pictures', 'Videos']
-	 * $c" Videos" = ['Music', 'Pictures', ' Videos']
-	 * "Videos"$b = ['Videos', 'Music', 'Pictures']
-	 * "Videos "$b = [' Videos', 'Music', 'Pictures']
-	 * "Videos"$a = ['VideosMusic', 'Pictures']
-	 * "Videos "$a = ['Videos Music', 'Pictures']
-	 * 
-	 * $c"Videos"$c$a
-	 * 
-	 * "asdf $b sd"$b
-	*/
-
-	(void)state;
-	(void)singlequote;
-
-	while(*ptr)
-	{
-
-		if (*ptr == '$')
-		{
-			
-		}
-		ptr++;
-	}
-}
-
-/**
  * In this implementation, if the next character after '$' is not
  * '_', alpha char or '?', both the first and second chars will be
  * skipped and not saved
@@ -185,6 +129,121 @@ void	sanitize_invalid_variables(t_token *token)
 	token->word = sanitized;
 }
 
+
+char	*find_var_name(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str[i] == '?')
+		return(ft_substr(str, 0, 1));
+	while(str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
+		i++;
+	return(ft_substr(str, 0, i));
+}
+
+char	*expand_inside_double_quotes(char *str, t_state *state)
+{
+	int	inside;
+	int	i;
+	char *name;
+	char *nb;
+
+	i = 0;
+	inside = 0;
+	
+	while(str[i])
+	{
+		if (str[i] == '\"')
+		{
+			if (!inside)
+				inside = 1;
+			else
+				inside = 0;
+		}
+		
+		if (inside && str[i] == '$')
+		{
+			name = find_var_name(str + i + 1);
+			if (*name == '?')
+			{
+				nb = ft_itoa(state->exit_status);
+				str = ft_expand(str, i, nb, ft_strlen(name));
+				free(nb);
+				return(expand_inside_double_quotes(str, state));
+			}
+
+			char *envari = ft_strjoin(name, "=");
+			if (ft_getenv(envari, state))
+			{
+				str = ft_expand(str, i, ft_getenv(envari, state), ft_strlen(name));
+				free(envari);
+				free(name);
+				return(expand_inside_double_quotes(str, state));
+			}
+			else
+			{
+				str = ft_del_non_var(str, i, ft_strlen(name));
+				free(name);
+				free(envari);
+				return (expand_inside_double_quotes(str, state));
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+
+/**
+ * Expand variables and $? (previous exit status)
+ * 
+ * In this implementation, if the next character after '$' is not
+ * '_', alpha char or '?', both the first and second chars will be
+ * skipped and not saved
+*/
+void ft_variable_expand(t_token *token, t_state *state)
+{
+	char	*ptr;
+	int		singlequote;
+
+	singlequote = 0;
+	ptr = token->word;
+
+	/**
+	 * $a = "Music Pictures"
+	 * $b = " Music Pictures"
+	 * $c = "Music Pictures "
+	 * 
+	 * $a"Videos" = ['Music', 'PicturesVideos']
+	 * $a" Videos" = ['Music', 'Pictures Videos']
+	 * $c"Videos" = ['Music', 'Pictures', 'Videos']
+	 * $c" Videos" = ['Music', 'Pictures', ' Videos']
+	 * "Videos"$b = ['Videos', 'Music', 'Pictures']
+	 * "Videos "$b = [' Videos', 'Music', 'Pictures']
+	 * "Videos"$a = ['VideosMusic', 'Pictures']
+	 * "Videos "$a = ['Videos Music', 'Pictures']
+	 * 
+	 * $c"Videos"$c$a = ['Music', 'Pictures', 'VideosMusic', 'Pictures', 'Music', 'Pictures']
+	 * "asdf $b sd"$b = ['asdf Music Pictures sd', 'Music', 'Pictures']
+	*/
+
+	// First expand inside double quotes
+
+	// expand outside quotes and handle dividing into new tokens
+	(void)state;
+	(void)singlequote;
+
+	while(*ptr)
+	{
+
+		if (*ptr == '$')
+		{
+			
+		}
+		ptr++;
+	}
+}
+
 // TODO: ft_remove_quotes();
 
 void	expand(t_state *state)
@@ -199,6 +258,7 @@ void	expand(t_state *state)
 		{
 			ft_tilde_expand(token, state);
 			sanitize_invalid_variables(token);
+			token->word = expand_inside_double_quotes(token->word, state);
 			//ft_variable_expand(token, state);
 		}
 		// ft_remove_quotes(token);
