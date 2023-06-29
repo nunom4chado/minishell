1628c3bcf14f06df888ef1ac327cde4771f16309
Nuno Miguel Martins Machado <numartin@c1r11s11.42porto.com>
2023-06-29 18:14:55 +0100

    refactor code

--- src/expand.c
+++ src/expand.c
@@ -6,152 +6,11 @@
 /*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:34:38 by numartin          #+#    #+#             */
-/*   Updated: 2023/06/29 14:31:30 by numartin         ###   ########.fr       */
+/*   Updated: 2023/06/29 17:26:39 by numartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-
-/**
- * TODO: DON'T expand if enclosed with single quotes
- *
- * echo $7asdfsasdf   output: asdfsasdf
-*/
-/*
-char	*expand(t_state *state)
-{
-	int i;
-	char	*envari;
-
-	i = 0;
-	while (state->cmd[i])
-	{
-		if (state->cmd[i] == '$')
-		{
-			envari = ft_read_until(state->cmd + i + 1);
-			if (ft_getenv(envari, state))
-			{
-				state->cmd = ft_expand_var(state->cmd, i, ft_getenv(envari, state), ft_strlen(envari) - 1);
-				return(expand(state));
-			}
-			else
-			{
-				state->cmd = skip_undefined_var(state->cmd, i, ft_strlen(envari) - 1);
-				return (expand(state));
-			}
-		}
-		i++;
-	}
-	return (state->cmd);
-}
-*/
-
-char	*expand_inside_double_quotes(char *str, t_state *state)
-{
-	int	inside;
-	int	i;
-	char *name;
-	char *nb;
-
-	i = 0;
-	inside = 0;
-
-	while(str[i])
-	{
-		if (str[i] == '\"')
-		{
-			if (!inside)
-				inside = 1;
-			else
-				inside = 0;
-		}
-
-		if (inside && str[i] == '$')
-		{
-			name = find_var_name(str + i + 1);
-			if (*name == '?')
-			{
-				nb = ft_itoa(state->exit_status);
-				str = ft_expand_var(str, i, nb, ft_strlen(name));
-				free(nb);
-				return(expand_inside_double_quotes(str, state));
-			}
-
-			char *envari = ft_strjoin(name, "=");
-			if (ft_getenv(envari, state))
-			{
-				str = ft_expand_var(str, i, ft_getenv(envari, state), ft_strlen(name));
-				free(envari);
-				free(name);
-				return(expand_inside_double_quotes(str, state));
-			}
-			else
-			{
-				str = skip_undefined_var(str, i, ft_strlen(name));
-				free(name);
-				free(envari);
-				return (expand_inside_double_quotes(str, state));
-			}
-		}
-		i++;
-	}
-	return (str);
-}
-
-/**
- * Expand variables and $? (previous exit status)
- *
- * In this implementation, if the next character after '$' is not
- * '_', alpha char or '?', both the first and second chars will be
- * skipped and not saved
-*/
-void ft_variable_expand(t_token *token, t_state *state)
-{
-	char	*ptr;
-	int		singlequote;
-
-	singlequote = 0;
-	ptr = token->word;
-
-	/**
-	 * $a = "Music Pictures"
-	 * $b = " Music Pictures"
-	 * $c = "Music Pictures "
-	 *
-	 * $a"Videos" = ['Music', 'PicturesVideos']
-	 * $a" Videos" = ['Music', 'Pictures Videos']
-	 * $c"Videos" = ['Music', 'Pictures', 'Videos']
-	 * $c" Videos" = ['Music', 'Pictures', ' Videos']
-	 * "Videos"$b = ['Videos', 'Music', 'Pictures']
-	 * "Videos "$b = [' Videos', 'Music', 'Pictures']
-	 * "Videos"$a = ['VideosMusic', 'Pictures']
-	 * "Videos "$a = ['Videos Music', 'Pictures']
-	 *
-	 * $c"Videos"$c$a = ['Music', 'Pictures', 'VideosMusic', 'Pictures', 'Music', 'Pictures']
-	 * "asdf $b sd"$b = ['asdf Music Pictures sd', 'Music', 'Pictures']
-	*/
-
-	// First expand inside double quotes
-
-	// expand outside quotes and handle dividing into new tokens
-	(void)state;
-	(void)singlequote;
-
-	while(*ptr)
-	{
-
-		if (*ptr == '$')
-		{
-
-		}
-		ptr++;
-	}
-}
-
-// TODO: ft_remove_quotes();
-
-
 
 void	expand_and_remove_quotes(t_token *token, t_state *state)
 {
@@ -231,12 +90,63 @@ void	expand_and_remove_quotes(t_token *token, t_state *state)
 	token->word = new;
 }
 
+/**
+ * Remove quotes inside a token.
+ *
+ * @note It will remove only quotes that should be removed,
+ * Eg. asdf"asd'sdf"sdf -> asdfasd'sdfsdf
+*/
+void	remove_quotes(t_token *token)
+{
+	char *old;
+	char *new;
+	char quote;
+	char *tmp;
 
+	old = token->word;
+	new = ft_calloc(1, 1);
+	quote = 0;
+	while(*old)
+	{
+		// toggle quote and advance to next position
+		if (ft_is_quote(*old))
+		{
+			if (quote == *old)
+			{
+				quote = 0;
+				old++;
+				continue ;
+			}
+			if (quote == 0)
+			{
+				quote = *old;
+				old++;
+				continue ;
+			}
+		}
+
+		// copy normal chars
+		tmp = malloc(ft_strlen(new) + 2);
+		ft_memcpy(tmp, new, ft_strlen(new));
+		tmp[ft_strlen(new)] = *old;
+		tmp[ft_strlen(new) + 1] = '\0';
+
+		free(new);
+		new = tmp;
+
+		old++;
+	}
+
+	free(token->word);
+	token->word = new;
+}
 
 /**
- * Expand tokens
+ * Apply variable expansions in token list
  *
- * NOTE: expansions will not occur when the token is an HEREDOC_DELIMITER,
+ * @param state pointer to the state struct
+ *
+ * @note Expansions will not occur when the token is an HEREDOC_DELIMITER,
  * only quote removal will be applied
 */
 void	expand(t_state *state)
@@ -246,13 +156,13 @@ void	expand(t_state *state)
 	token = state->tokens;
 	while (token)
 	{
-		if (token->type != HEREDOC_DELIMITER)
+		if (token->type == HEREDOC_DELIMITER)
+			remove_quotes(token);
+		else
 		{
 			ft_tilde_expand(token, state);
 			expand_and_remove_quotes(token, state);
 		}
-		// ft_remove_quotes(token);
-		// TODO: clear empty tokens (some tokens may be left with only nullbyte - "")
 		token = token->next;
 	}
 }
--- src/expand_utils.c
+++ src/expand_utils.c
@@ -6,55 +6,34 @@
 /*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 17:38:22 by numartin          #+#    #+#             */
-/*   Updated: 2023/06/28 15:52:04 by numartin         ###   ########.fr       */
+/*   Updated: 2023/06/29 17:42:13 by numartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * If a variable is not in env, skip that parte and join both ends
+ * Find the full name of the variable
  *
- * @param str char * cointaining a portion to be removed
- * @param start position to start removing
- * @param end position to stop removing
+ * @param str a pointer to the next character after the '$'
  *
- * @return char * of the resulted join
+ * @return char * with the name of the var
+ *
+ * @note If the variable has an invalid name, it will return the first char.
+ * This is necessary to know the length to skip when iterating a string.
 */
-char	*skip_undefined_var(char *str, int start, int end)
+char	*find_var_name(char *str)
 {
-	char	*new;
+	int i;
 
-	new = malloc(ft_strlen(str) - end + start + 1);
-	ft_memcpy(new, str, start);
-	new[start] = '\0';
-	ft_strcat(new, str + start + 1 + end);
-	free(str);
-	return (new);
-}
-
-/**
- * @brief Replace the variable name with the variable value
- *
- * @param str the original char *
- * @param expandable the char * to be inserted
- * @param start position to start replacing
- * @param end position to stop replacing
- *
- * @return pointer to the resulting string
- *
-*/
-char	*ft_expand_var(char *str, int start, char *expandable, int end)
-{
-	char	*ptr;
-
-	ptr = malloc(ft_strlen(str) + ft_strlen(expandable) - 1);
-	ft_memcpy(ptr, str, start);
-	ptr[start] = '\0';
-	ft_strcat(ptr, expandable);
-	ft_strcat(ptr, str + start + 1 + end);
-	free(str);
-	return (ptr);
+	i = 0;
+	if (str[i] == '?')
+		return(ft_substr(str, 0, 1));
+	if (!ft_isalpha(str[i]) && str[i] != '_')
+		return(ft_substr(str, 0, 1));
+	while(str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
+		i++;
+	return(ft_substr(str, 0, i));
 }
 
 /**
@@ -79,66 +58,4 @@ void ft_tilde_expand(t_token *token, t_state *state)
 			token->word = tmp;
 		}
 	}
-}
-
-/**
- * Remove invalid variable from the token
- *
- * @note In this implementation, if the next character after '$' is not
- * '_', alpha char or '?', both the first and second chars will be
- * skipped and not saved
- *
- * @param token pointer to a token
-*/
-void	sanitize_invalid_variables(t_token *token)
-{
-	char *sanitized;
-	char *aux;
-	char *old;
-
-	old = token->word;
-	sanitized = malloc(ft_strlen(old) + 1);
-	// TODO: handle error
-	aux = sanitized;
-	while (*old)
-	{
-		if (*old == '$' && *(old + 1))
-		{
-			if(!ft_isalpha(*(old + 1)) && *(old + 1) != '_' && *(old + 1) != '?')
-			{
-				old = old + 2;
-				continue ;
-			}
-		}
-		*aux = *old;
-		aux++;
-		old++;
-	}
-	*aux = '\0';
-	free(token->word);
-	token->word = sanitized;
-}
-
-/**
- * Find the full name of the variable
- *
- * @param str a pointer to the next character after the '$'
- *
- * @return char * with the name of the var
- *
- * @note If the variable has an invalid name, it will return the first char.
- * This is necessary to know the length to skip when iterating a string.
-*/
-char	*find_var_name(char *str)
-{
-	int i;
-
-	i = 0;
-	if (str[i] == '?')
-		return(ft_substr(str, 0, 1));
-	if (!ft_isalpha(str[i]) && str[i] != '_')
-		return(ft_substr(str, 0, 1));
-	while(str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
-		i++;
-	return(ft_substr(str, 0, i));
 }
