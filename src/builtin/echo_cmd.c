1c0419e8f1c75eb2a5a1bf61f9a231670d2b693a
nunom4chado <nunom4chado@gmail.com>
2023-07-14 14:15:29 +0100

    norm echo

--- src/builtin/echo_cmd.c
+++ src/builtin/echo_cmd.c
@@ -6,7 +6,7 @@
 /*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 13:26:10 by numartin          #+#    #+#             */
-/*   Updated: 2023/07/14 13:28:04 by numartin         ###   ########.fr       */
+/*   Updated: 2023/07/14 14:14:16 by numartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,40 @@
 
 extern t_state		g_state;
 
+/**
+ * Print a series of words separated by spaces.
+ * Last word will NOT have a space.
+ * 
+ * At the end, if -n flag was set, don't print the newline
+*/
+void	print_words(char **word, int i, int nl_flag)
+{
+	while (word[i])
+	{
+		printf("%s", word[i]);
+		if (word [i + 1])
+			printf(" ");
+		i++;
+	}
+	if (nl_flag)
+		printf("\n");
+}
+
+/**
+ * Execut builtin echo
+ * 
+ * @note -n flag is supported (will not print a newline at the end,
+ * making the prompt appear in the same line)
+*/
 void	builtin_echo(char **cmd)
 {
 	int	i;
-	int	nl;
+	int	nl_flag;
 
 	if (g_state.echo)
 	{
 		i = 0;
-		nl = 1;
+		nl_flag = 1;
 		if (!cmd || !cmd[i])
 		{
 			printf("\n");
@@ -30,18 +55,10 @@ void	builtin_echo(char **cmd)
 		}
 		while (ft_strcmp(cmd[i], "-n") == 0)
 		{
-			nl = 0;
+			nl_flag = 0;
 			i++;
 		}
-		while (cmd[i])
-		{
-			printf("%s", cmd[i]);
-			if (cmd [i + 1])
-				printf(" ");
-			i++;
-		}
-		if (nl)
-			printf("\n");
+		print_words(cmd, i, nl_flag);
 		g_state.exit_status = 0;
 	}
 	g_state.echo = 1;
