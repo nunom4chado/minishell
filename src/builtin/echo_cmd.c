d291dabd02bab33f20ba86d22991acdcc8f6bfd3
Nuno Miguel Martins Machado <numartin@c1r11s11.42porto.com>
2023-07-15 11:31:44 +0100

    fix echo invalid read

--- src/builtin/echo_cmd.c
+++ src/builtin/echo_cmd.c
@@ -6,7 +6,7 @@
 /*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 13:26:10 by numartin          #+#    #+#             */
-/*   Updated: 2023/07/14 14:14:16 by numartin         ###   ########.fr       */
+/*   Updated: 2023/07/15 11:31:10 by numartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ extern t_state		g_state;
 /**
  * Print a series of words separated by spaces.
  * Last word will NOT have a space.
- * 
+ *
  * At the end, if -n flag was set, don't print the newline
 */
 void	print_words(char **word, int i, int nl_flag)
@@ -35,7 +35,7 @@ void	print_words(char **word, int i, int nl_flag)
 
 /**
  * Execut builtin echo
- * 
+ *
  * @note -n flag is supported (will not print a newline at the end,
  * making the prompt appear in the same line)
 */
@@ -53,7 +53,7 @@ void	builtin_echo(char **cmd)
 			printf("\n");
 			return ;
 		}
-		while (ft_strcmp(cmd[i], "-n") == 0)
+		while (cmd[i] && ft_strcmp(cmd[i], "-n") == 0)
 		{
 			nl_flag = 0;
 			i++;
